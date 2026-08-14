6c64ca66fa285f2ac89f4255878a6703f983805a
MatheusGrisard <matheusgrisard@gmail.com>
2026-08-14 20:28:54 -0300

    Aula 1: configuração inicial do projeto

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import http from "node:http";
+
+const port = 3000;
+
+const server = http.createServer((reg, res) =>{
+    const response = {
+        message: "API Restaurante",
+        version: "1.0.0",
+        xpto: "qualquer coisa..."
+    };
+
+    res.writeHead(200, {
+        "content-type": "application/json", 
+    });
+
+    res.end(JSON.stringify(response));
+});
+
+server.listen(port, () => {
+    console.log(`Servidor executando em http://localhost:${port}`);
+})
